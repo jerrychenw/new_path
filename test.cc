c9b205bd23a8bcd7c051a65c13ccc5619ae7f303
Xing Yan <xingyan@Xings-MacBook-Pro.local>
2020-06-24 00:56:39 -0400

    change test

--- test.cc
+++ test.cc
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+void PrintVec(const std::vector<int>& vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << vec[i] <<  ((i == vec.size() - 1) ? "\n" : " ");
+    }
+}
 
 int main() {
-    std::cout << "This is test" << std::endl;
+    std::vector<int> to_be_sorted{1, 3, 16, 3};
+    PrintVec(to_be_sorted);
+    std::sort(to_be_sorted.begin(), to_be_sorted.end());
+    PrintVec(to_be_sorted);
 }
